6b7700c91996d6a55b59901f9a0b8558776499c3
Temidire <temidireowoeye@gmail.com>
2022-12-12 20:15:06 +0100

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARSYLCavmIPu5sX3xdl_i_ZMX_r861x60",
+  authDomain: "chapter40-jewellery.firebaseapp.com",
+  projectId: "chapter40-jewellery",
+  storageBucket: "chapter40-jewellery.appspot.com",
+  messagingSenderId: "965935510438",
+  appId: "1:965935510438:web:53e6a71e7498004d8c1567",
+  measurementId: "G-CE95N62KH9",
+};
