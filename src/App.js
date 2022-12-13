93946ea73edd1a0026eef40cc4778d27559740a0
Temidire <temidireowoeye@gmail.com>
2022-12-13 09:21:06 +0100

    created header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the website for Chapter40</h1>
+    <div>
+      <Header />
     </div>
   );
 }
