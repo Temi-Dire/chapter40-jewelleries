924b5c49d62f89578b43d13de88ff1bee29ffd45
Temidire <temidireowoeye@gmail.com>
2023-01-11 05:00:49 +0100

    feat: created function to get the total price

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,14 +1,24 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../StateProvider";
 
 function Subtotal() {
+  //eslint-disable-next-line
+  const [{ basket }, dispatch] = useStateValue();
+  function getTotal() {
+    let sum = 0;
+    for (let i = 0; i < basket.length; i++) {
+      sum += basket[i].price;
+    }
+    return sum;
+  }
   return (
     <div className="flex flex-col justify-between w-full md:w-[300px] h-[150px] p-[20px] bg-[#f3f3f3] border border-solid border-[#dddddd] rounded">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" name="" id="" /> This order contains a gift
@@ -16,7 +26,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getTotal()}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"₦"}
