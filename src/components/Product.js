103c6abfee21755f6cb63d77151198cd60e7d59c
Temidire <temidireowoeye@gmail.com>
2023-01-11 05:01:58 +0100

    feat: function to dispatch items to data layer

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
 //eslint-disable-next-line
-function Product({ image, alt, desc, price, ratings }) {
+function Product({ image, id, desc, price, ratings }) {
+  //eslint-disable-next-line
+  const [state, dispatch] = useStateValue();
+
+  function addToBasket() {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        desc: desc,
+        price: price,
+      },
+    });
+  }
+
   return (
     <div className="w-full ">
       {/* <div className="w-[200px] md:w-[250px]"> */}
@@ -8,12 +23,12 @@ function Product({ image, alt, desc, price, ratings }) {
       <div className="w-full h-[200px] bg-red-500 rounded-t-lg"></div>
       {/* <img
           src={image}
-          alt={alt}
+          alt="Product"
           className="w-full max-w-[250px] h-[200px] md:h-[250px] rounded-t-lg"
         /> */}
       <div className="border-solid border-2 border-t-0 border-grey rounded-b-lg p-2">
         <p>{desc}</p>
-        <p>{price}</p>
+        <p>{"₦" + price}</p>
         {/* <div className="flex flex-row">
           {Array(ratings)
             .fill()
@@ -21,7 +36,10 @@ function Product({ image, alt, desc, price, ratings }) {
               <p>⭐</p>
             ))}
         </div> */}
-        <button className="bg-red-50 rounded p-2 w-full mt-2">
+        <button
+          className="bg-red-50 rounded p-2 w-full mt-2"
+          onClick={addToBasket}
+        >
           Add To Cart
         </button>
       </div>
