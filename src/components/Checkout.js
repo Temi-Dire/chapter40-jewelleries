d76f80bbb921aa91f577433c0458a2125967d55f
Temidire <temidireowoeye@gmail.com>
2023-01-11 04:59:07 +0100

    initialized checkout product in the checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useStateValue } from "../StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  //eslint-disable-next-line
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="md:flex p-[20px]">
       <div className="checkout__left w-full mr-4 border-2 border-solid border-[grey] mb-2 md:mb-0">
-        This is a checkout page
+        <CheckoutProduct />
       </div>
       <div className="checkout__right">
         <Subtotal />
