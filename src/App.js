cf23cdd75ab945334c650160b9b72fdc1977db47
Temidire <temidireowoeye@gmail.com>
2023-01-11 04:59:48 +0100

    feat: initialized react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Header from "./components/Header";
 import Home from "./components/Home";
+import Header from "./components/Header";
+import Checkout from "./components/Checkout";
+//eslint-disable-next-line
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
-    </div>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
