14a14e169efa664547686890158f87e66652809a
Temidire <temidireowoeye@gmail.com>
2023-02-26 20:27:42 +0100

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+// import { deepOrange } from "@mui/material/colors";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
 import { onAuthStateChanged, signOut } from "firebase/auth";
@@ -33,13 +34,13 @@ function Header() {
   };
 
   return (
-    <div className="sticky top-0 bg-[white]">
+    <div className="sticky top-0 bg-white">
       {/* ${nav_bar}  */}
       <div
         className={`flex justify-between mx-4 py-2 md:mx-8 sm:flex sm:items-center`}
       >
         <Link to={"/"}>
-          <div className="flex flex-col font-['Aclonica'] text-2xl italic  text-primary">
+          <div className="flex flex-col font-['Aclonica'] text-2xl italic  ">
             {/* Change the font-style of logo */}
             <span>CHAPTER40 </span>
             <span>JEWELLERY</span>
@@ -48,24 +49,22 @@ function Header() {
         <div className="sm:hidden">
           <div className="flex items-center h-full justify-between font-['Rubik'] mx-[10px] ">
             <Link to="/necklaces">
-              <span className="mr-8 hover:text-primary hover:text-primary">
-                Necklaces
-              </span>
+              <span className="mr-8 hover:text-primary ">Necklaces</span>
             </Link>
             <Link to="/earrings">
-              <span className="mr-8 hover:text-primary">Earrings</span>
+              <span className="mr-8 ">Earrings</span>
             </Link>
             <Link to="/rings">
-              <span className="mr-8 hover:text-primary">Rings</span>
+              <span className="mr-8 ">Rings</span>
             </Link>
             <Link to="/watches">
-              <span className="hover:text-primary">Watches</span>
+              <span className="">Watches</span>
             </Link>
           </div>
         </div>
         <div className="inline flex items-center sm:hidden">
           <Link to={!authUser && "/login"}>
-            <div className="flex flex-col leading-loose">
+            <div className="flex flex-col leading-loose ">
               <span className="mr-8 text-[13px] font-['Rubik']">
                 Hello {authUser ? authUser.email.split("@")[0] : "Guest"},
               </span>
@@ -83,7 +82,10 @@ function Header() {
             <span className="mr-8 text-[13px] font-['Rubik']">& Orders</span>
           </div>
           <Link to={"/checkout"}>
-            <ShoppingCartIcon className="ml-2" />
+            <ShoppingCartIcon
+              // sx={{ color: deepOrange[500] }}
+              className="ml-2"
+            />
           </Link>
           <span>{basket?.length}</span>
         </div>
@@ -93,7 +95,7 @@ function Header() {
           <Link to={"/checkout"}>
             <div className="flex">
               <ShoppingCartIcon
-                // sx={{ color: amber[100] }}
+                // sx={{ color: deepOrange[500] }}
                 class="w-8 h-8 flex"
               />
               {basket?.length}
