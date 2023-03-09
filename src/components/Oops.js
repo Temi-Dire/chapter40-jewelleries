a172208d5ebf18831d5a26538c9a07d3d982ff51
Temidire <temidireowoeye@gmail.com>
2023-03-09 21:34:50 +0100

    added error msg to login page

--- src/components/Oops.js
+++ src/components/Oops.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 function Oops({ msg, desc, btn, click }) {
   return (
@@ -7,9 +6,9 @@ function Oops({ msg, desc, btn, click }) {
       <div className="flex flex-col justify-center items-center">
         <div className="w-4/5 md:w-3/5  flex flex-col justify-center items-center rounded-[20px]">
           <p className="font-bold mb-4">{msg}</p>
-          <p className="font-bold mb-8">{desc}</p>
+          <p className="font-bold mb-8 text-center">{desc}</p>
           <button
-            className=" bg-inherit text-[#060825] border-[#060825] hover:text-white hover:bg-[#060825] border-2 py-2 px-3 text-lg font-bold rounded-xl flex justify-center items-center "
+            className=" bg-inherit w-3/5 text-[#060825] border-[#060825] hover:text-white hover:bg-[#060825] border-2 py-2 px-3 text-lg font-bold rounded-xl flex justify-center items-center "
             onClick={click}
           >
             {btn}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,10 +3,12 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 import { motion as m } from "framer-motion";
+import Oops from "../components/Oops";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [noSuchUser, setNoSuchUser] = useState(false);
 
   const navigate = useNavigate();
 
@@ -20,64 +22,77 @@ function Login() {
         }
       })
       .catch((error) => {
-        console.error(error.message);
+        setNoSuchUser(true);
       });
   };
 
   return (
     <div>
-      <div className="h-screen sm:flex sm:justify-center sm:items-center md:grid md:grid-cols-2 md:items-center md:justify-items-center">
-        <div className="sm:hidden h-screen w-full bg-white"></div>
-        <div className="md:w-7/10 p-8">
-          <h1 className="sm:mb-8 text-3xl sm:font-bold font-extrabold sm:w-[70%] sm:my-0text-center mb-4">
-            Welcome
-          </h1>
-          <h1 className="sm:mb-8 sm:text-xl sm:font-bold  mb-4">
-            Please login to your account
-          </h1>
-          <form action="" onSubmit={login}>
-            <m.input
-              whileFocus={{ borderColor: "black" }}
-              className="w-full h-16 rounded-lg bg-[##FAFAFA] outline-none py-5 px-4 border border-[#E3E0E0] mb-10"
-              type="email"
-              placeholder="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+      {!noSuchUser ? (
+        <div className="h-screen sm:flex sm:justify-center items-center md:grid md:grid-cols-2  md:justify-items-center">
+          <div className="sm:hidden h-screen w-full bg-white"></div>
+          <div className="md:w-7/10 p-8">
+            <h1 className="sm:mb-8 text-3xl sm:font-bold font-extrabold sm:w-[70%] sm:my-0text-center mb-4">
+              Welcome
+            </h1>
+            <h1 className="sm:mb-8 sm:text-xl sm:font-bold  mb-4">
+              Please login to your account
+            </h1>
+            <form action="" onSubmit={login}>
+              <m.input
+                whileFocus={{ borderColor: "black" }}
+                className="w-full h-16 rounded-lg bg-[##FAFAFA] outline-none py-5 px-4 border border-[#E3E0E0] mb-10"
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-            <m.input
-              whileFocus={{ borderColor: "black" }}
-              className="w-full h-16 rounded-lg bg-[##FAFAFA] outline-none py-5 px-4 border border-[#E3E0E0] mb-10"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <div className="flex justify-center">
+              <m.input
+                whileFocus={{ borderColor: "black" }}
+                className="w-full h-16 rounded-lg bg-[##FAFAFA] outline-none py-5 px-4 border border-[#E3E0E0] mb-10"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="flex justify-center">
+                <m.button
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ duration: 0.5 }}
+                  className="bg-white border border-black w-3/5 p-2 rounded-[50px]  hover:bg-black hover:text-white"
+                  type="submit"
+                >
+                  Login
+                </m.button>
+              </div>
+            </form>
+            <div className="text-center mt-4 ">
+              <span>Don't have an account?</span>
               <m.button
                 whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ duration: 0.5 }}
-                className="bg-white border border-black w-3/5 p-2 rounded-[50px]  hover:bg-black hover:text-white"
-                type="submit"
+                transition={{ duration: 0.3 }}
+                className="ml-4 font-bold"
+                onClick={() => navigate("/signup")}
               >
-                Login
+                Create One
               </m.button>
             </div>
-          </form>
-          <div className="text-center mt-4 ">
-            <span>Don't have an account?</span>
-            <m.button
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.3 }}
-              className="ml-4 font-bold"
-              onClick={() => navigate("/signup")}
-            >
-              Create One
-            </m.button>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="h-screen flex justify-center items-center ">
+          <Oops
+            msg={"OOPS"}
+            desc={
+              "Looks like you've entered an incorrect login information. Please if you don't have an account, you can create one now"
+            }
+            click={() => setNoSuchUser(false)}
+            btn={"Ok"}
+          />
+        </div>
+      )}
     </div>
   );
 }
