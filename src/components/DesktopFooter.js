c16b7e127c7a906c22f72fe75472d88d23b1974f
Temidire <temidireowoeye@gmail.com>
2023-04-19 09:37:12 +0100

    created file for desktop footer

---
+++ src/components/DesktopFooter.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function DesktopFooter() {
+  return (
+    <div className="flex mt-4 md:m-0">
+							<div className="-mx-4">
+								<a
+									href="#"
+									className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline"
+								>
+									Twitter
+								</a>
+								<a
+									href="#"
+									className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline"
+								>
+									Instagram
+								</a>
+								<a
+									href="#"
+									className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline"
+								>
+									Partners
+								</a>
+								<a
+									href="#"
+									className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline"
+								>
+									Contact Us 📧
+								</a>
+								<a
+									href="#"
+									className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline"
+								>
+									We're hiring ✨
+								</a>
+							</div>
+						</div>
+  )
+}
+
+export default DesktopFooter
